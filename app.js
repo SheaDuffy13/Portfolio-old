275bd295d628e3b6ad1a5fd10740b45ce0d5825d
SheaDuffy13 <13420@coderacademy.edu.au>
2022-08-24 04:13:25 +1000

    Linked blogs, about-me page laid out, global CSS

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const navSlide = ()=>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links a');
-    //Toggle Nav
+
     burger.addEventListener('click', ()=>{
+        //Toggle Nav
         nav.classList.toggle('nav-active');
-    });
-    //Animate Links
-    navLinks.forEach((link, index)=>{
-        link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7 + 2)s';
-        console.log(index / 7);
+
+        //Animate Links
+        navLinks.forEach((link, index)=>{
+            if(link.getElementsByClassName.animation) {
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards $(index / 7 + 1.5)s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
     });
 }
 
